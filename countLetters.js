fe10f0ef35182040281eddb58d5a3f3b053790a6
Benton <chan.benton+test@gmail.com>
2017-02-08 18:10:43 +0000

    Final copy, revised

--- countLetters.js
+++ countLetters.js
@@ -1,33 +1,13 @@
-// Write a function countLetters that can return to us all the 
-// unique characters that exist in a string that is passed into the function.
-//   Example output:
-//   {
-//   l: 1,
-//   i: 2,
-//   g: 1,
-//   h: 4,
-//   t: 2,
-//   o: 2,
-//   u: 2,
-//   s: 2,
-//   e: 3,
-//   n: 1,
-// }
-
-
 function countLetters(string){
 	var letterList = string.split("");
 	var breakdown = {};
 
 	for (currentLetterIndex in letterList){
 		var letter = letterList[currentLetterIndex];
-
-		if (breakdown[letter]){
-			breakdown[letter] += 1;
+		if (!breakdown[letter]){
+			breakdown[letter] = 0;
 		}
-		else{
-			breakdown[letter] = 1;	
-		}
+		breakdown[letter] += 1;
 	}
 	return breakdown;
 }
